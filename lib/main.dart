d2c1fe62625fff8ec6d79e8417dd434c678230a2
鄭會憲 <vkcel400@gmail.com>
2024-01-30 18:36:06 +0900

    temp

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
