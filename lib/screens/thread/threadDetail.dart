f48ed1d801d695a460f0629da69201418da62b6f
鄭會憲 <vkcel400@gmail.com>
2024-02-11 19:31:13 +0900

    20240211

--- lib/screens/thread/threadDetail.dart
+++ lib/screens/thread/threadDetail.dart
@@ -1,4 +1,5 @@
-import 'dart:developer';
+import 'dart:developer' as dev;
+import 'dart:math';
 
 import 'package:find_friend/providers/thread.dart';
 import 'package:find_friend/providers/userInfo.dart';
@@ -13,78 +14,142 @@ class ThreadDetailScreen extends StatelessWidget {
   final ThreadProvider threadProvider = Get.put(ThreadProvider());
   final UserInfoProvider userInfoProvider = Get.put(UserInfoProvider());
 
+  final List<String> messages = [];
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      const CustomBackGroundImageWidget(
-        type: 'bg',
-      ),
-      Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: CustomTextWidget(
-            text: Get.arguments.title,
-            kind: 'headTitle2',
-          ),
+    dev.log('ThreadDetailScreen build ${Get.arguments.title}');
+    return Stack(
+      children: [
+        const CustomBackGroundImageWidget(type: 'bg'),
+        Scaffold(
           backgroundColor: Colors.transparent,
-          automaticallyImplyLeading: true,
-          iconTheme: const IconThemeData(
-            color: Colors.black54,
+          appBar: AppBar(
+            iconTheme: const IconThemeData(color: Colors.black),
+            backgroundColor: Colors.transparent,
+            title: CustomTextWidget(
+              text: Get.arguments.title,
+              kind: 'label',
+            ),
           ),
-          actions: _renderAppbarActions(),
-        ),
-        body: Center(
-          child: Text(
-            'Thread Detail Screen \n ${Get.arguments.id}',
-            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                  color: Colors.black,
+          body: Column(
+            children: <Widget>[
+              Flexible(
+                child: ListView.builder(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.all(10.0),
+                  reverse: true,
+                  itemCount: 40,
+                  itemBuilder: (_, int index) =>
+                      _renderMessageItem(context, 'messages[index] $index'),
                 ),
+              ),
+              const Divider(height: 1.0),
+              _buildTextComposer(context),
+            ],
           ),
-        ),
-      )
-    ]);
+        )
+      ],
+    );
   }
 
-  List<Widget>? _renderAppbarActions() {
-    // TODO: 방을 작성한 사람은 해당 방을 삭제할 권한을 가진다
+  Widget _renderMessageItem(BuildContext context, String message) {
+    var intValue = Random().nextInt(10);
 
-    // return null;
-    return [
-      IconButton(
-        icon: const Icon(Icons.remove_circle_outline_outlined),
-        color: Colors.red,
-        style: ButtonStyle(
-          overlayColor: MaterialStateProperty.all(Colors.red[200]),
-          iconSize: MaterialStateProperty.all(30),
-        ),
-        onPressed: () {
-          Get.dialog(
-            AlertDialog(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              title: const Text('スレッtを削除しますか？'),
-              content: const Text('このすれっとを削除すると元に戻すことはできません\n本当に削除しますか？'),
-              actions: <Widget>[
-                TextButton(
-                  child: const Text('Close'),
-                  onPressed: () {
-                    log('close');
-                    Get.back();
-                  },
-                ),
-                TextButton(
-                  child: const Text('OK'),
-                  onPressed: () {
-                    log('delete thread');
-                    Get.back();
-                  },
-                ),
-              ],
+    if (intValue.isEven) {
+      return Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          mainAxisAlignment:
+              intValue.isEven ? MainAxisAlignment.start : MainAxisAlignment.end,
+          children: <Widget>[
+            const CircleAvatar(
+              child: Text('A'),
             ),
-          );
-        },
-      )
-    ].toList();
+            const SizedBox(
+              width: 3,
+            ),
+            Card(
+              color: Colors.blue,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(message),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        mainAxisAlignment:
+            intValue.isEven ? MainAxisAlignment.start : MainAxisAlignment.end,
+        children: <Widget>[
+          Card(
+            color: Colors.blue,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(message),
+            ),
+          ),
+          const SizedBox(
+            width: 3,
+          ),
+          const CircleAvatar(
+            child: Text('B'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextComposer(BuildContext context) {
+    return Container(
+      color: Colors.transparent,
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              maxLines: 3,
+              minLines: 1,
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Colors.black,
+                  ),
+              keyboardType: TextInputType.multiline,
+              controller: _textController,
+              onSubmitted: (value) {
+                dev.log('onSubmitted: $value');
+              },
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Send a message',
+                hintStyle: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              icon: const Icon(Icons.send),
+              color: Colors.blue,
+              onPressed: () {
+                dev.log(_textController.text);
+                _textController.clear();
+                _scrollController.animateTo(
+                  _scrollController.position.maxScrollExtent,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
