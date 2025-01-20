a91857176deb8f8a71f3983bb3a55bf0259d13ef
鄭會憲 <vkcel400@gmail.com>
2025-01-21 06:28:14 +0900

    sssss

--- lib/utils/googleAd.dart
+++ lib/utils/googleAd.dart
@@ -1,18 +1,1 @@
-import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-
-class GoogleAdManager {
-  // 리워드 광고 키값
-  static String getRewardAdUnitId() {
-    if (Platform.isAndroid) {
-      return kReleaseMode
-          ? 'ca-app-pub-9674517651101637/6682220543'
-          : 'ca-app-pub-3940256099942544/5224354917';
-    } else if (Platform.isIOS) {
-      return kReleaseMode ? '' : 'ca-app-pub-3940256099942544/1712485313';
-    } else {
-      return 'none';
-    }
-  }
-}
